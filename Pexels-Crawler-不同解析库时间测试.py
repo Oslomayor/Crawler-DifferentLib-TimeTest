6a0868f61a1eb372a0bef954b6fc11c201884263
Denis <34310801+Oslomayor@users.noreply.github.com>
2018-03-06 20:13:30 +0800

    四个解析方法的时间对比

--- Pexels-Crawler-不同解析库时间测试.py
+++ Pexels-Crawler-不同解析库时间测试.py
@@ -4,6 +4,7 @@
 # 使用不同的解析库测试效率
 # https://www.pexels.com/
 
+import re
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -12,46 +13,55 @@ from lxml import etree
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 
-def BS_htmlparser(keyword):
+
+# 装饰器怎么写？
+def log(fn):
+    print('call ' + fn.__name__)
+    return fn
+
+# 获取 res 后再用不同的库解析，避免网速的波动影响
+def get_res(keyword):
     url = 'https://www.pexels.com/search/{}'.format(keyword)
-    res = requests.get(url, headers=headers)
+    return  requests.get(url, headers=headers)
+
+def BS_htmlparser(res):
     soup = BeautifulSoup(res.text, 'html.parser')
     jpg_srcs = soup.select('body > div.page-wrap > div.l-container > div.photos > article > a > img')
     for jpg_src in jpg_srcs:
         jpg_src = jpg_src.get('src')
-        print(jpg_src)
+        # print(jpg_src)
 
-def BS_lxml(keyword):
-    url = 'https://www.pexels.com/search/{}'.format(keyword)
-    res = requests.get(url, headers=headers)
+def BS_lxml(res):
     soup = BeautifulSoup(res.text, 'lxml')
     jpg_srcs = soup.select('body > div.page-wrap > div.l-container > div.photos > article > a > img')
     for jpg_src in jpg_srcs:
         jpg_src = jpg_src.get('src')
-        print(jpg_src)
+        # print(jpg_src)
 
-# unfinished
-def Lxml_Xpath(keyword):
-    url = 'https://www.pexels.com/search/{}'.format(keyword)
-    res = requests.get(url, headers=headers)
+def Lxml_Xpath(res):
     selector = etree.HTML(res.text)
-    # 郁闷，刚刚想写 Xpath ，登不上 pexels 网站了
-    infos = selector.xpath('//article')
+    # XPath 语法其实挺简单，img 节点中 src 后的链接，写成 /img/@src
+    infos = selector.xpath('/html/body/div[1]/div[2]/div[3]/article')
     for info in infos:
-        # Xpath 应该怎么写？
-        info = info.xpath('/a[1]/img') #/html/body/div[2]/div[3]/article[1]/a[1]/img
-        print(info)
+        #第二个 Xpath 选取节点不需要加斜杠 /
+        info = info.xpath('a[1]/img/@src')
+        # print(info[0])
 
-# unfinished
+def re_findall(res):
+    infos = re.findall(' src=\"https://(.*?)\" />', res.text)
+    for info in infos:
+        info = 'https://' + info
+        # print(info)
+
 def main():
     keyword = input('Please input keyword:\n')
-    # for crawler in [BS_htmlparser, BS_lxml, Lxml_Xpath, re]:
-    #     time_start = time.time()
-    #     crawler(keyword)
-    #     time_end = time.time()
-    #     print()
-    crawler = Lxml_Xpath
-    crawler(keyword)
+    res = get_res(keyword)
+    for crawler in [BS_htmlparser, BS_lxml, Lxml_Xpath, re_findall]:
+        time_start = time.time()
+        crawler(res)
+        time_end = time.time()
+        print(crawler.__name__, '=', time_end - time_start, '秒' , '\n')
+
 
 if __name__ == '__main__':
     main()
